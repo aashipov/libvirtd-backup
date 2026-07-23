4ec60b414bad50a1270dab6ab9baff68163dc11f
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2026-07-23 03:19:26 +0300

    bc.sh

--- bc.sh
+++ bc.sh
@@ -1,71 +1,94 @@
 #!/bin/sh
 
-# DESCRIPTION:
-#   Backs libvirtd machines up.
+# ------------------------------------------------------------
+#  bc.sh – Live‑disk backup for libvirt VMs
+# ------------------------------------------------------------
+#  Requires:
+#    - libvirt ≥ 7.2.0
+#    - QEMU ≥ 4.2
+#    - sh (POSIX‑compliant shell)
 #
-# USAGE:
-#   ./bc.sh
-#
-# OPTIONS:
-#   no options intended
-#
+#  Configuration (via .env):
+#    BACKUP_DIR          – local directory for backups
+#    BACKUP_LOG_FILE     – file to append log messages
+#    VM_NAMES_TO_BACK_UP – space separated list of VM names
+# ------------------------------------------------------------
 # SEE ALSO:
 #   https://libvirt.org/kbase/live_full_disk_backup.html
 #
 # NOTE:
 #   `virsh --connect qemu:///system domjobabort ${VM_NAME}` aborts backup task for a given ${VM_NAME}
 
-# append to ${BACKUP_LOG_FILE}
+# ------------------------------------------------------------
+#  Utility helpers
+# ------------------------------------------------------------
 log() {
-    printf "$(date '+%Y-%m-%d %H:%M:%S') - ${1}\n" | tee -a ${BACKUP_LOG_FILE}
+    # Append timestamped message to log file and optionally to stdout
+    printf '%s - %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "${1}" | tee -a "${BACKUP_LOG_FILE}"
 }
 
-# Loads environment variables from .env
+die() {
+    # Log and exit
+    printf '%s - %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "${1}" | tee -a "${BACKUP_LOG_FILE}"
+    exit 1
+}
+
+# ------------------------------------------------------------
+#  Environment loading
+# ------------------------------------------------------------
 environment() {
-    # . for bash, zsh, ksh, (d)ash. source for (t)csh
-    if [ ! -f ./.env ]
+    # Loads environment variables from .env
+    local _SCRIPT_DIR=`dirname -- $(readlink -f -- "$0")`
+    local ENV_FILE=${_SCRIPT_DIR}/.env
+    if [ ! -f ${ENV_FILE} ]
     then
-        printf "No .env file found, craft one from .env.template\n"
-        exit 1
+        die "No ${ENV_FILE} file found, craft one from ${ENV_FILE}.template"
     fi
-    . ./.env
+    # . for bash, zsh, ksh, (d)ash. source for (t)csh
+    . ${ENV_FILE}
     mkdir -p ${BACKUP_DIR}
 }
 
-# Creates a local backup dir if missing
 create_backup_dir() {
+    # Creates a local backup dir if missing
     mkdir -p ${BACKUP_DIR}
 }
 
-# Creates a ${BACKUP_DIR}/YYYY-mm-dd for the current run of the script
 create_current_backup_dir() {
+    # Creates a ${BACKUP_DIR}/YYYY-mm-dd for the current run of the script
     mkdir -p ${CURRENT_BACKUP_DIR}
 }
 
-# if marker file ${RUNNING_FILE} exists
+# ------------------------------------------------------------
+#  Marker/lock file
+# ------------------------------------------------------------
 check_running() {
+    # if marker/lock file ${RUNNING_FILE} exists
     if [ -f ${RUNNING_FILE} ]
     then
-        log "Another copy of this file may be running. Stop it, remove ${RUNNING_FILE} and repeat. Exiting\n"
-        exit 1
+        die "Another copy of this file may be running. Stop it, remove ${RUNNING_FILE} and repeat. Exiting"
     fi
 }
 
-# creates a marker file ${RUNNING_FILE}
 create_running() {
+    # creates a marker/lock file ${RUNNING_FILE}
     touch ${RUNNING_FILE}
 }
 
-# removes the marker file ${RUNNING_FILE}
 rm_running() {
+    # removes the marker/lock file ${RUNNING_FILE}
     rm ${RUNNING_FILE}
 }
 
-backup_vm() {
+# ------------------------------------------------------------
+#  Back the running VM up
+# ------------------------------------------------------------
+backup_running_vm() {
+    # Exports VM configuration (XML) and copies disks
     local VM_NAME=${1}
     # Dump VM config
-    virsh --connect qemu:///system dumpxml ${VM_NAME} > ${CURRENT_BACKUP_DIR}/${VM_NAME}.xml
-    local DOMBLKLIST=`virsh --connect qemu:///system domblklist ${VM_NAME} --details | grep disk`
+    virsh --connect qemu:///system dumpxml ${VM_NAME} > ${CURRENT_BACKUP_DIR}/${VM_NAME}.xml || die "Failed to dump an XML config for ${VM_NAME}"
+    local DOMBLKLIST=`virsh --connect qemu:///system domblklist ${VM_NAME} --details | grep disk || die "Could not parse disk list for ${VM_NAME}" `
 
     local DISK_NAMES=`printf "${DOMBLKLIST}\n" | tr -s ' ' | cut -d ' ' -f 4 | tr '\n' ' ' | sed 's/[[:space:]]*$//'`
 
@@ -73,7 +96,7 @@ backup_vm() {
     local BACKUP_TASK_XML="<domainbackup>\n    <disks>"
     for DISK_NAME in ${DISK_NAMES}
     do
-        BACKUP_TASK_XML="${BACKUP_TASK_XML}\n        <disk name='${DISK_NAME}' type='file'>\n            <target file='/${CURRENT_BACKUP_DIR}/${VM_NAME}-${DISK_NAME}.qcow2'/>\n                <driver type='qcow2'/>\n        </disk>\n"
+        BACKUP_TASK_XML="${BACKUP_TASK_XML}\n        <disk name='${DISK_NAME}' type='file'>\n            <target file='${CURRENT_BACKUP_DIR}/${VM_NAME}-${DISK_NAME}.qcow2'/>\n                <driver type='qcow2'/>\n        </disk>\n"
     done
     BACKUP_TASK_XML="${BACKUP_TASK_XML}    </disks>\n</domainbackup>"
 
@@ -81,34 +104,29 @@ backup_vm() {
     local BACKUP_TASK_FILE=${CURRENT_BACKUP_DIR}/${VM_NAME}-backup-task.xml
     printf "${BACKUP_TASK_XML}\n" > ${BACKUP_TASK_FILE}
 
-    # launch backup
-    virsh --connect qemu:///system backup-begin ${VM_NAME} --backupxml ${BACKUP_TASK_FILE}
+    # launch
+    log "${VM_NAME} backup start"
+    virsh --connect qemu:///system backup-begin ${VM_NAME} --backupxml ${BACKUP_TASK_FILE} ||
+            die "Failed to start backup for ${VM_NAME}"
 
     # wait for backup completion
-    while true; do
-        if `virsh --connect qemu:///system domjobinfo ${VM_NAME} | grep -q "None"`
+    while :; do
+        if virsh --connect qemu:///system domjobinfo ${VM_NAME} | grep -q "None"
         then
             break
         fi
         sleep 10s
     done
-    log "${VM_NAME} backup completed"
+    log "${VM_NAME} backup finish"
 }
 
-# Returns space separated list for a ${1} VM name
-list_vm_hard_disks() {
-    local VM_NAME=${1}
-    local DOMBLKLIST=`virsh --connect qemu:///system domblklist ${VM_NAME} --details | grep disk`
-    local DISK_NAMES=`printf "${DOMBLKLIST}\n" | tr -s ' ' | cut -d ' ' -f 4 | tr '\n' ' ' | sed 's/[[:space:]]*$//'`
-    printf "${DISK_NAMES}"
-}
-
-# main function to prevent occasional environment pollution
+# ------------------------------------------------------------
+#  Main function to prevent occasional environment pollution
+# ------------------------------------------------------------
 closure() {
-    set -Eeuo pipefail
     #set -x # debug
     environment
-    create_backup_dir # at this point log file is available
+    create_backup_dir # at this point log file must be available
 
     local LAUNCH_DATE=`date +%Y-%m-%d`
     local CURRENT_BACKUP_DIR=${BACKUP_DIR}/${LAUNCH_DATE}
@@ -116,21 +134,19 @@ closure() {
     check_running
     create_current_backup_dir
 
+    log "Backup start"
     create_running
-
     for VM_NAME_TO_BACK_UP in ${VM_NAMES_TO_BACK_UP}
     do
-        if `virsh --connect qemu:///system domstate ${VM_NAME_TO_BACK_UP} | grep -q "running"`
+        if virsh --connect qemu:///system domstate ${VM_NAME_TO_BACK_UP} | grep -q "running"
         then
-           backup_vm ${VM_NAME_TO_BACK_UP}
+           backup_running_vm ${VM_NAME_TO_BACK_UP}
         else
-            log "${VM_NAME_TO_BACK_UP} is not running, skipping\n"
+            log "${VM_NAME_TO_BACK_UP} is not running, skipping"
         fi
     done
-
     rm_running
-
-    log "Backup done"
+    log "Backup finish"
 }
 
 closure
